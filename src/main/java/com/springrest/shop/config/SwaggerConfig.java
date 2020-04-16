c896975fabc4ed7bc8c3b8596963a5093046ab5d
George <gtsianakas94@gmail.com>
2020-04-16 12:55:25 +0300

    Update SwaggerConfig.java v2
    Customizing Swagger with Meta Data

--- src/main/java/com/springrest/shop/config/SwaggerConfig.java
+++ src/main/java/com/springrest/shop/config/SwaggerConfig.java
@@ -2,34 +2,45 @@ package com.springrest.shop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig { //} extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
 
-//    @Override
-//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
+    private ApiInfo metaData() {
+
+        Contact contact = new Contact("George", " ",
+                "gtsianakas94@gmail.com");
+
+        return new ApiInfo(
+                "Spring Fruit Shop API",
+                "Spring Fruit Shop API Implementation",
+                "1.0",
+                "Terms of Service: blah blah blah",
+                contact,
+                " ",
+                " ",
+                new ArrayList<>());
+            }
 
 }
